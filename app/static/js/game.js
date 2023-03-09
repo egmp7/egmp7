c447aa4c31b3d5f5cfd590cc87dca77b518272b8
egmp7 <deweyerick@gmail.com>
2023-03-09 15:27:48 -0600

    game.js code clean

--- app/static/js/game.js
+++ app/static/js/game.js
@@ -1,30 +1,6 @@
-var gameChar_x;
-var gameChar_y;
 var floorPos_y;
 var scrollPos;
-var gameChar_world_x;
 var player;
-
-var clouds = [];
-var mountains;
-var trees;
-var canyons;
-var collectables;
-var platforms;
-var enemies;
-var flag_pole;
-
-var gameStart =false;
-var game_score;
-var lives;
-var token;
-
-var isLeft;
-var isRight;
-var isFalling;
-var isPlummeting;
-
-var sounds;
 
 function setup()
 {
@@ -37,44 +13,16 @@ function setup()
 function draw()
 {
 	drawBackground();
-    drawStatus();
-    
-    // Translate Elements
     push();
     translate(scrollPos,0);
-    
     levelAssets.forEach((asset)=>{
         asset.draw();
-    })
-    
-    // Save
+    })    
     pop();
-    
-    // Renders: Start Game, Level Completed or Game Over texts.
-    // if(drawGameEvents()){
-    //     return
-    // };
-
-    //drawGameChar();
-    player.draw()
-        
-    // Update real position of gameChar for collision detection.
-
+    player.draw()    
 }
-// ---------------------
-// START
-// ---------------------
-
-// ----------------------------
-// KEY CONTROL FUNCTIONS
-// ---------------------------
 
 
-// Function that respond to key events
-
-// ---------------------------------
-// VIEW RENDERING FUNCTIONS
-// ---------------------------------
 // Function to draw the Background
 function drawBackground (){
     background(100, 155, 255); // fill the sky blue
@@ -82,48 +30,53 @@ function drawBackground (){
 	fill(0,155,0);
 	rect(0, floorPos_y, width, height/4); // draw some green ground 
 }
+
 // Function to draw lives tokens and Score
-function drawStatus (){
+// function drawStatus (){
     
-    // Draw Score
-    fill(250);
-    textSize(20);
-    textFont('Courier')
-    textAlign(LEFT);
-    text('Lives: ',50,30);
-    text('Score: '+game_score,50,55)
+//     // Draw Score
+//     fill(250);
+//     textSize(20);
+//     textFont('Courier')
+//     textAlign(LEFT);
+//     text('Lives: ',50,30);
+//     text('Score: '+game_score,50,55)
     
-    // Draw Tokens:
-    for(var i=0;i<lives;i++){
-        var x = 120 + i*23
-        var y = 15
+//     // Draw Tokens:
+//     for(var i=0;i<lives;i++){
+//         var x = 120 + i*23
+//         var y = 15
         
-        fill(0,0,200);
-        triangle(x,y,x+20,y,x+10,y+20);
-        fill(255,255,102);
-        triangle(x+8,y+5,x+12,y+5,x+10,y+15);
-    }
+//         fill(0,0,200);
+//         triangle(x,y,x+20,y,x+10,y+20);
+//         fill(255,255,102);
+//         triangle(x+8,y+5,x+12,y+5,x+10,y+15);
+//     }
     
-}
+// }
+
+
+
+
 // Function to draw Start Level, Level Completed or Game Over
-function drawGameEvents(){
-   textAlign(CENTER);
-    fill(255);
-    // Start Level
-    if(gameStart == false){
-        text("Press Enter to start",width/2,height/2);
-        return true;
-    }
-    //Level Completed
-    else if(flag_pole.isReached == true){
-        text("Level complete. Press Enter to continue.",width/2,height/2);
-        return true;
-    }
-    // Game Over
-    else if(lives<1){
-        text("Game over. Press Enter to continue.",width/2,height/2);
-        return true;
-    }
-}
+// function drawGameEvents(){
+//    textAlign(CENTER);
+//     fill(255);
+//     // Start Level
+//     if(gameStart == false){
+//         text("Press Enter to start",width/2,height/2);
+//         return true;
+//     }
+//     //Level Completed
+//     else if(flag_pole.isReached == true){
+//         text("Level complete. Press Enter to continue.",width/2,height/2);
+//         return true;
+//     }
+//     // Game Over
+//     else if(lives<1){
+//         text("Game over. Press Enter to continue.",width/2,height/2);
+//         return true;
+//     }
+// }
 
 
