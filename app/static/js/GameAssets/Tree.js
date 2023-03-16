0b693e8aa1b8414625672e21f6318249d7537c0e
egmp7 <deweyerick@gmail.com>
2023-03-16 00:36:05 -0500

    tree back

--- app/static/js/GameAssets/Tree.js
+++ app/static/js/GameAssets/Tree.js
@@ -1,23 +1,25 @@
 class Tree 
 {
-    constructor (x)
+    height = 124;
+
+    constructor ( _x, _y )
     {
-
-        this.x = x;
-        this.y = -70;
-
+        this.x = _x;
+        this.y = _y - this.height;
     }
 
     draw = function ()
     {
-        fill(102,51,0)
-        rect(this.x,floorPos_y + this.y,20,70);
-        fill(0,102,0)
-        triangle(this.x-20,floorPos_y + this.y,
-        this.x+40,floorPos_y + this.y,
-        this.x+10,floorPos_y-30 + this.y);
-        triangle(this.x-20,floorPos_y-15 + this.y,
-        this.x+40,floorPos_y-15 + this.y,
-        this.x+10,floorPos_y-45 + this.y);     
+        fill( 102, 51, 0 )
+        rect(( this.x ), ( this.y + 48 ), ( 20 ), ( this.height - 48 ) );
+        fill( 0, 102, 0 )
+        triangle( 
+            ( this.x - 20 ), ( this.y + 48 ),
+            ( this.x + 40 ), ( this.y + 48 ),
+            ( this.x + 10 ), ( this.y + 16 ));
+        triangle( 
+            ( this.x - 20 ), ( this.y + 32),
+            ( this.x + 40 ), ( this.y + 32),
+            ( this.x + 10 ), ( this.y ));     
     }
 }
