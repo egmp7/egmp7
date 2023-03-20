92943df108fdef47b9c25eff1ff4420d6ae22682
egmp7 <deweyerick@gmail.com>
2023-03-20 14:02:06 -0500

    0 lives menu

--- app/static/js/Rules.js
+++ app/static/js/Rules.js
@@ -2,9 +2,13 @@ class Rules
 {
     static playerDead = function ()
     {
+        
+        STATUS.setLives( STATUS.getLives() - 1 )
+        if (STATUS.getLives() < 1)
+            MENU.setActive(true)
+
         PLAYER_CONTROLLER.setScrollPos ( 0 )
         PLAYER_CONTROLLER.setWorldX( 300 )
         PLAYER_CONTROLLER.setY( height / 2 )
-        STATUS.setLives( STATUS.getLives() - 1 )
     }
 }
