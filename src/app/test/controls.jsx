51e31029018f0f460a99d78ae76178a62892cb7a
egmp7 <deweyerick@gmail.com>
2023-08-31 23:54:09 -0500

    control component, touch  buttons

---
+++ src/app/test/controls.jsx
@@ -0,0 +1,94 @@
+import dynamic from 'next/dynamic'
+const Sketch = dynamic(() => import("react-p5"), { ssr: false });
+
+const buttonWidth = 50;
+const buttonHeight = 50;
+
+export default function Controls({ name }) {
+
+    const setup = (p5, canvasParentRef) => {
+
+        p5.createCanvas(buttonWidth, buttonHeight).parent(canvasParentRef)
+        p5.ellipseMode(p5.CORNER);
+        p5.noStroke();
+        drawButtons(p5,name)
+
+    }
+
+    function touchStart() {
+        console.log(`touch start ${name}`)
+        //Body.setVelocity(player.physics, Vector.create(1, 0))
+    }
+
+    function touchEnd() {
+        console.log(`touch end ${name}`)
+        //Body.setVelocity(player.physics, Vector.create(-1, 0))
+    }
+
+    return (
+        <button
+            className={classNames(
+                name === "left" ? "top-[400px] left-[30px]" :
+                name === "right" ? "top-[400px] left-[100px]" :
+                name === "jump" ? "top-[400px] left-[250px]" :
+                "", `absolute rounded-full w-[${buttonWidth}px] h-[${buttonHeight}px] `)}
+            onTouchStart={e => touchStart()}
+            onTouchEnd={e => touchEnd()}
+            onContextMenu={(e) => e.preventDefault()}>
+            <Sketch setup={setup} />
+        </button>)
+}
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+function drawButtons(p5, name) {
+    if (name === "left") {
+        p5.fill('rgba(255,255,255, 0.25)');
+        p5.ellipse(0, 0, buttonWidth, buttonHeight)
+        const size = buttonWidth / 2 - 10;
+        p5.fill(0, 255, 0);
+        p5.push();
+        p5.translate(20, 25);
+        p5.triangle(
+            +size, +size,
+            +size, -size,
+            -size, 0);
+        p5.pop();
+    }
+
+    if (name === "right") {
+        p5.fill('rgba(255,255,255, 0.25)');
+        p5.ellipse(0, 0, buttonWidth, buttonHeight)
+
+        const size = buttonWidth / 2 - 10;
+        p5.fill(0, 255, 0);
+        p5.push();
+        p5.translate(30, 25);
+        p5.triangle(
+            -size, -size,
+            -size, +size,
+            +size, 0);
+        p5.pop();
+    }
+
+    if (name === "jump") {
+        p5.fill('rgba(255,255,255, 0.25)');
+        p5.ellipse(0, 0, buttonWidth, buttonHeight)
+
+        const size = buttonWidth / 2 - 12;
+        p5.fill(0, 255, 0);
+        p5.push();
+        p5.translate(26, 25);
+        p5.strokeWeight(6)
+        p5.stroke(0, 0, 180)
+        p5.line(
+            - size, - size,
+            + size, + size)
+        p5.line(
+            + size, - size,
+            - size, + size)
+        p5.pop();
+    }
+}
--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -6,36 +6,27 @@ import {
   Engine,
   Bodies,
   Composite,
-  Body,
-  Vector
 } from "matter-js";
-
 import { Player } from './player.js'
-
-// Dynamic import
+import Controls from './controls.jsx';
 const Sketch = dynamic(() => import("react-p5"), { ssr: false });
 
 var engine;
 var ground;
-
 let width = 500;
 let height = 500;
-var boxA;
-var boxB;
 var ground;
-var cnv;
+var player;
 var sound = new Howl({
   src: ['/tictoc.mp3']
 });
-
-var player;
 
 export default function Game() {
 
   const setup = (p5, canvasParentRef) => {
 
     // CANVAS
-    cnv = p5.createCanvas(width, height).parent(canvasParentRef)
+    p5.createCanvas(width, height).parent(canvasParentRef)
     // MATTER
     // create an engine
     engine = Engine.create();
@@ -44,11 +35,11 @@ export default function Game() {
     player = new Player(200, 100);
     // add all of the bodies to the world
     Composite.add(engine.world, [player.physics, ground]);
-
+    
   }
 
   const draw = (p5) => {
-    cnv.background(255, 130, 20)
+    p5.background(255, 130, 20)
     Engine.update(engine);
 
     drawVertices(p5, player.physics.vertices)
@@ -56,26 +47,20 @@ export default function Game() {
     player.run(p5)
   };
 
-  function touchStart() {
-    console.log("you touch starts")
-    Body.setVelocity(player.physics, Vector.create(1, 0))
-  }
-
-  function touchEnd() {
-    console.log("you touch ends")
-    Body.setVelocity(player.physics, Vector.create(-1, 0))
-  }
-
   return (
     <>
-      <Sketch setup={setup} draw={draw} />
-      <button onTouchStart={e => touchStart()} onTouchEnd={e => touchEnd()}>Tap me</button>
+      <div className="relative">
+        <Controls name={"left"}/>
+        <Controls name={"right"}/>
+        <Controls name={"jump"}/>
+        <Sketch setup={setup} draw={draw} />
+      </div>
     </>);
 }
 
 // Utility
 const drawVertices = function (p5, vertices) {
-  cnv.fill(255, 0, 0)
+  p5.fill(255, 0, 0)
   p5.beginShape();
   for (var i = 0; i < vertices.length; i++) {
     p5.vertex(vertices[i].x, vertices[i].y);
