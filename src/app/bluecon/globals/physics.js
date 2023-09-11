663e2e969d24ee846890bc96b7a2a011e6133a16
egmp7 <deweyerick@gmail.com>
2023-09-11 18:25:31 -0500

    enemy Collision

--- src/app/bluecon/globals/physics.js
+++ src/app/bluecon/globals/physics.js
@@ -6,24 +6,40 @@ class Physics {
     constructor() {
         this.engine = Engine.create();
         this.collisions = {
-            ground: false
+            ground: false,
+            enemy: false
         }
     }
 
     run() {
-        this.groundCollisions(player.floorSensor, LevelOneBodies.grounds)
+        this.groundCollision(player.floorSensor, LevelOneBodies.grounds)
+        this.enemyCollision(player.body, LevelOneBodies.enemies)
+
     }
 
-    groundCollisions(floorSensor, grounds) {
+    groundCollision(playerFloorSensor, grounds) {
         this.collisions.ground = false
         grounds.forEach(ground => {
-            if (Collision.collides(floorSensor, ground))
+            if (Collision.collides(playerFloorSensor, ground))
                 this.collisions.ground = true
+        });
+    }
+
+    enemyCollision(playerBody, enemies) {
+        this.collisions.enemy = true
+        enemies.forEach(enemy => {
+            if (Collision.collides(playerBody, enemy))
+                //this.collisions.enemy = true
+                console.log("enemy collision")
         });
     }
 
     isPlayerOnGround() {
         return this.collisions.ground;
+    }
+
+    isEnemyCollision(){
+        return this.collisions.enemy;
     }
 }
 
