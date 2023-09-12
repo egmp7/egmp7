d8782b29fabe7516d98081b7ea4d96db801d79bd
egmp7 <deweyerick@gmail.com>
2023-09-11 20:13:31 -0500

    scroll refactoring

--- src/app/bluecon/globals/scrollPos.js
+++ src/app/bluecon/globals/scrollPos.js
@@ -1,37 +1,42 @@
 import player from "../assets/playerBody"
 import LevelOneBodies from "../levels/levelOneBodies"
-import {Body} from "matter-js" 
+import { Body } from "matter-js"
 
 class ScrollPos {
-    constructor(){
-        this.position={x:0,y:0}
+    constructor() {
+        this.position = { x: 0, y: 0 }
     }
-    run(){
-        this.scroll(player.main.position,this.position)
+    run() {
+        this.checkScroll(player.main.position, this.position)
     }
 
-    scroll(playerPosition,scrollPos){
+    checkScroll(playerPosition) {
         const xRightLimit = 400;
         const xLeftLimit = 50;
         const speed = 6;
-        if (playerPosition.x > xRightLimit){
-            Body.setPosition(player.main,{x:xRightLimit, y:player.main.position.y})
-            this.moveBodies(LevelOneBodies.grounds, - speed)
-            this.moveBodies(LevelOneBodies.enemies, - speed)
-        }
-        if (playerPosition.x < xLeftLimit){
-            Body.setPosition(player.main,{x:xLeftLimit, y:player.main.position.y})
-            this.moveBodies(LevelOneBodies.grounds, speed)
-            this.moveBodies(LevelOneBodies.enemies, speed)
-        }
-            
+        if (playerPosition.x < xLeftLimit)
+            this.scroll(xLeftLimit, +speed)
+        if (playerPosition.x > xRightLimit)
+            this.scroll(xRightLimit, -speed)
     }
 
-    moveBodies(bodies, speed){
-        
-        bodies.forEach(body => {
-            Body.setPosition(body,{x:body.position.x + speed , y:body.position.y })
-        });
+    scroll(limit, speed) {
+        Body.setPosition(player.main, {
+            x: limit,
+            y: player.main.position.y
+        })
+        this.moveBodies(LevelOneBodies, speed)
+        this.position.x += speed
+    }
+
+    moveBodies(allBodies, speed) {
+
+        for (const property in allBodies) {
+
+            allBodies[property].forEach(body => {
+                Body.setPosition(body, { x: body.position.x + speed, y: body.position.y })
+            });
+        }
     }
 }
 
