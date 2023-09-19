673428820e144ef2349c5f9d83b5c93ed8590364
egmp7 <deweyerick@gmail.com>
2023-09-19 17:37:25 -0500

    switch Velocity function

--- test/enemy.js
+++ test/enemy.js
@@ -7,25 +7,35 @@ describe("Set of test for an Enemy", () => {
         var x = 5;
         var range = 10;
         var speed = 6;
+        var velocity = speed
 
         it("should not accept negative range", () => {
-            assert.throws(() => { switchSpeed(x, -range) }, "error: range must not be less than 0")
+            assert.throws(() => { switchVelocity(x, -range) }, "error: range must not be less than 0")
         })
 
-        it("should change the speed to -6 if xEnemyCurrent is bigger than xEnemyInit + range", () => {
-            switchSpeed(x, range, 16)
-            assert.equal(speed, -6)
+        it("should change the velocity to -6 if xEnemyCurrent is bigger than xEnemyInit + range", () => {
+            switchVelocity(x, range, 16);
+            switchVelocity(x, range, 17);
+            assert.equal(velocity, -6)
         })
 
-        it("should change the speed to 6 if xEnemyCurrent is less than xEnemyInit", () => {
-            switchSpeed(x, range, 4)
-            assert.equal(speed, 6)
+        it("should change the velocity to 6 if xEnemyCurrent is less than xEnemyInit", () => {
+            switchVelocity(x, range, 4);
+            switchVelocity(x, range, 3);
+            assert.equal(velocity, 6)
         })
 
-        function switchSpeed(xEnemyInit, range, xEnemyCurrent) {
+
+        /**
+         * Switches the speed when the enemy passes a threshold
+         * @param {number} xEnemyInit 
+         * @param {number} range 
+         * @param {number} xEnemyCurrent 
+         */
+        function switchVelocity(xEnemyInit, range, xEnemyCurrent) {
             if (range < 0) throw "error: range must not be less than 0";
-            if (xEnemyCurrent > xEnemyInit + range) speed = -6;
-            if (xEnemyCurrent < xEnemyInit) speed = 6;
+            if (xEnemyCurrent > xEnemyInit + range) velocity = -speed;
+            if (xEnemyCurrent < xEnemyInit) velocity = speed;
         }
 
     })
