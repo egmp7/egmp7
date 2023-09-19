524082257e60447eddb2c6ca97e334392b13f8ef
egmp7 <deweyerick@gmail.com>
2023-09-19 18:44:42 -0500

    refactoring enemy

--- src/app/bluecon/assets/enemy.js
+++ src/app/bluecon/assets/enemy.js
@@ -6,25 +6,18 @@ import physics from "../globals/physics";
 export default class Enemy {
     constructor(body, range) {
         this.body = body;
-
-        Body.setInertia(body, Infinity)
-        this.x = body.position.x;
+        this.xInit = body.position.x;
         this.range = range;
-        this.currentX = body.position.x;
-        this.inc = 1;
+        this.speed = 2;
+        Body.setInertia(body, Infinity)
+        Body.setVelocity(body, { x: this.speed, y: this.body.velocity.y })
     }
 
     run(p5) {
         drawVertices(p5, this.body.vertices)
         this.reverseGravity(physics.engine.gravity, this.body)
-        this.updateEnemyPosition(
-            this.body,
-            this.currentX,
-            this.x,
-            this.range,
-            this.inc);
-
-        this.draw(p5, this.body.position)
+        this.switchVelocity(this.xInit, this.range, this.body.position.x, this.speed);
+        this.draw(p5, this.body.position, this.body.velocity)
     }
 
     /**
@@ -41,54 +34,33 @@ export default class Enemy {
     }
 
     /**
-     * Moves the position of the body depending on the range
-     * @param {Matter.Body} body 
-     * @param {number} currentX 
-     * @param {number} x 
-     * @param {number} range 
-     * @param {number} inc 
-     */
-    updateEnemyPosition = function (body, currentX, x, range, inc) {
-        const speed = 2;
-        this.setCurrentX(currentX + inc);
-
-        if (currentX >= x + range) {
-            Body.setVelocity(body, Vector.create(-speed, 0));
-            this.setInc(-1);
-        }
-        else if (currentX < x) {
-            Body.setVelocity(body, Vector.create(speed, 0));
-            this.setInc(1);
-        }
-    }
-
-    /**
-     * Set Increment value
-     * @param {number} value 
-     */
-    setInc(value) {
-        this.inc = value;
-    }
-    /**
-     * Set current x value
-     * @param {number} value 
-     */
-    setCurrentX(value) {
-        this.currentX = value;
+         * Switches the speed when the enemy passes a threshold
+         * @param {number} xEnemyInit 
+         * @param {number} range 
+         * @param {number} xEnemyCurrent
+         * @param {number} speed 
+         */
+    switchVelocity(xEnemyInit, range, xEnemyCurrent, speed) {
+        if (range < 0) throw "error: range must not be less than 0";
+        if (xEnemyCurrent > xEnemyInit + range)
+            Body.setVelocity(this.body, { x: -speed, y: this.body.velocity.y })
+        if (xEnemyCurrent < xEnemyInit)
+            Body.setVelocity(this.body, { x: speed, y: this.body.velocity.y })
     }
 
     /**
      * Draws an enemy
      * @param {SketchProps.p5} p5 
-     * @param {Matter.Vector} position 
+     * @param {Matter.Vector} position
+     * @param {Matter.Vector} velocity 
      */
-    draw = function (p5, position) {
+    draw = function (p5, position, velocity) {
 
         const yOffset = 3;
 
         p5.push();
         p5.translate(position.x, position.y + yOffset);
-        if (this.inc < 0) drawEnemyLeft(p5)
+        if (velocity.x < 0) drawEnemyLeft(p5)
         else drawEnemyRight(p5)
         p5.pop()
     }
