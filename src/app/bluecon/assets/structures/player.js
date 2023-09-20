80107d4ef17164d50b5436e82e79411c1182605e
egmp7 <deweyerick@gmail.com>
2023-09-20 10:42:37 -0500

    refactoring player p2

--- src/app/bluecon/assets/structures/player.js
+++ src/app/bluecon/assets/structures/player.js
@@ -14,6 +14,9 @@ export default class Player extends Structure {
 
     constructor() {
         super(player.main);
+        this.xSpeed = 5;
+        this.jumpForce = { x: 0, y: (-0.013 * this.body.mass) };
+
     }
 
     canDoubleJump = { isInAir: false, isFirstJump: false };
@@ -21,26 +24,34 @@ export default class Player extends Structure {
     run = function (p5) {
 
         //drawVertices(p5, this.body.vertices);
-        this.draw(p5, this.body.position);
-        this.moveSides();
-        this.jump();
+        this.draw(p5, this.body.position, this.control, this.isPlayerOnGround());
+        this.moveSides({ x: this.xSpeed, y: this.body.velocity.y }, this.control);
+        this.jump(this.jumpForce, this.control.jump, this.isPlayerOnGround());
         this.doubleJump();
     }
 
-    moveSides = function () {
-        const speed = 5;
-        const velocityRight = { x: speed, y: this.body.velocity.y }
-        const velocityLeft = { x: -speed, y: this.body.velocity.y }
-        const xVelocityZero = { x: 0, y: this.body.velocity.y }
-        if (this.control.right) this.setVelocity(velocityRight);
-        else if (this.control.left) this.setVelocity(velocityLeft);
-        else this.setVelocity(xVelocityZero);
+    /**
+     * Move the player side ways
+     * @param {Vector} velocity 
+     * @param {Control.Object} control 
+     */
+    moveSides = function (velocity, control) {
+        const velocityRight = { x: velocity.x, y: velocity.y }
+        const velocityLeft = { x: -velocity.x, y: velocity.y }
+        const velocityCenter = { x: 0, y: velocity.y }
+        if (control.right) this.setVelocity(velocityRight);
+        else if (control.left) this.setVelocity(velocityLeft);
+        else this.setVelocity(velocityCenter);
     }
 
-    jump = function () {
-        const force = { x: 0, y: (-0.013 * this.body.mass) };
-        if (this.control.jump && this.isPlayerOnGround())
-            this.applyForce(force)
+    /**
+     * Makes the player Jump
+     * @param {Vector} force 
+     * @param {Boolean} isJump 
+     * @param {Boolean} isOnGround 
+     */
+    jump = function (force, isJump, isOnGround) {
+        if (isJump && isOnGround) this.applyForce(force)
     }
 
     doubleJump = function () {
@@ -68,16 +79,23 @@ export default class Player extends Structure {
             this.canDoubleJump.isFirstJump = true;
     }
 
-    draw = function (p5, position) {
+    /**
+     * 
+     * @param {P5} p5 
+     * @param {Vector} position 
+     * @param {Control.Object} control 
+     * @param {Boolean} isOnGround 
+     */
+    draw = function (p5, position, control, isOnGround) {
         const yOffset = -3
         p5.push()
         p5.translate(position.x, position.y + yOffset)
         p5.scale(1.7)
-        if (this.control.left && !this.isPlayerOnGround()) leftFallingAnimation(p5)
-        else if (this.control.right && !this.isPlayerOnGround()) rightFallingAnimation(p5)
-        else if (this.control.left) leftAnimation(p5)
-        else if (this.control.right) rightAnimation(p5)
-        else if (!this.isPlayerOnGround()) fallingAnimation(p5)
+        if (control.left && !isOnGround) leftFallingAnimation(p5)
+        else if (control.right && !isOnGround) rightFallingAnimation(p5)
+        else if (control.left) leftAnimation(p5)
+        else if (control.right) rightAnimation(p5)
+        else if (!isOnGround) fallingAnimation(p5)
         else frontAnimation(p5)
         p5.pop()
     }
