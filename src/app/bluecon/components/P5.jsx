8139d5e74d0bf114b8c33197fc569eed19c18140
egmp7 <deweyerick@gmail.com>
2023-09-22 11:53:54 -0500

    p5Globals class

--- src/app/bluecon/components/P5.jsx
+++ src/app/bluecon/components/P5.jsx
@@ -1,14 +1,16 @@
 'use client';
 import dynamic from 'next/dynamic'
 import Game from '../game';
-import {setGlobalP5} from '../globals/p5';
+import {setGlobalP5, P5Globals , canvas} from '../globals/p5';
 
 const Sketch = dynamic(() => import("react-p5"), { ssr: false });
 
 export default function P5() {
-  const game = new Game();
+  
   const canvasWidth = 960;
   const canvasHeight = 540;
+  var p5Globals;
+  var game;
 
   const setup = (p5, canvasParentRef) => {
     const cnv = p5.createCanvas(canvasWidth, canvasHeight).parent(canvasParentRef);
@@ -18,6 +20,11 @@ export default function P5() {
     cnv.style("width", "100%");
     cnv.style("height", "100%");
     setGlobalP5(p5);
+
+    game = new Game();
+    p5Globals = new P5Globals(cnv);
+    
+    
   }
 
   const draw = (p5) => {
