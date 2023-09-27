8d0907b8e6467c49be2d9a1bd42f5ee072196ef3
egmp7 <deweyerick@gmail.com>
2023-09-27 07:23:30 -0500

    scrolling structures

--- src/app/bluecon/modules/Scroll.ts
+++ src/app/bluecon/modules/Scroll.ts
@@ -2,22 +2,49 @@ import Loader from "./Loader";
 //////////////////////////////////////////////////////////
 import type Matter from "matter-js";
 import type Player from "../assets/structures/player";
-import type Structure from "../abstract/structure";
+import Structure from "../abstract/structure";
 //////////////////////////////////////////////////////////
 namespace Scroll {
     let position: Matter.Vector = { x: 0, y: 0 }
     let player: Player;
     let allStructuresNoPlayer: Structure[]
+    let scrollingSpeed = 6;
     const xRightLimit = 400;
     const xLeftLimit = 50;
 
-    export function init(){
+    export function init() {
         allStructuresNoPlayer = Loader.getAllStructuresNoPlayer();
         player = Loader.getPlayer();
     }
 
-    export function run(){
+    export function run() {
 
+        // left canvas limit
+        if (player.body.position.x < xLeftLimit) {
+            updateScroll(scrollingSpeed);
+            player.setPosition({ x: xLeftLimit, y: player.body.position.y });
+        }
+        // right canvas limit
+        if (player.body.position.x > xRightLimit) {
+            updateScroll(- scrollingSpeed);
+            player.setPosition({ x: xRightLimit, y: player.body.position.y })
+        }
+
+        scrollStructures(allStructuresNoPlayer);
+    }
+
+    function scrollStructures(structures: Structure[]): void {
+        structures.forEach((structure) => {
+            structure.setPosition({
+                x: structure.position.x + position.x,
+                y: structure.body.position.y
+            })
+        })
+    }
+
+    function updateScroll(speed: number): void {
+        position.x += speed;
+        console.log(position)
     }
 }
 
