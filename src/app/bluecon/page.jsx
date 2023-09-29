2270e4bb69c4270388d6ac49461d791c2a22049a
egmp7 <deweyerick@gmail.com>
2023-09-29 00:12:01 -0500

    main sketch controlling the frame ratio

--- src/app/bluecon/page.jsx
+++ src/app/bluecon/page.jsx
@@ -12,9 +12,7 @@ export default function GameSketch() {
       <div className='flex flex-col min-h-screen '>
         <div className='my-auto'>
           <div className='max-w-screen-lg mx-auto'>
-            <div className="relative pb-[56.25%] max-m-[43.75%] h-0">
               <Sketch />
-            </div>
           </div>
         </div>
       </div>
