63f1d4f1bc2e4eed4e521c29ea8383701aa9c1af
egmp7 <deweyerick@gmail.com>
2023-09-30 00:44:45 -0500

    max width for large devices, and hide overflow bug

--- src/app/bluecon/page.jsx
+++ src/app/bluecon/page.jsx
@@ -9,10 +9,12 @@ export default function GameSketch() {
 
   return (
     <>
-      <div className='select-none' contenteditable="false">
-        <div className='absolute aspect-video  h-screen  translate-x-1/2 right-1/2'>
-          <div className='absolute bg-blue-400 w-full aspect-video max-w-[100vw] translate-y-1/2 bottom-1/2 translate-x-1/2 right-1/2'>
-            <Sketch />
+      <div className='relative select-none' contenteditable="false">
+        <div className='absolute w-screen h-screen overflow-hidden'>
+          <div className='absolute aspect-video  h-screen  translate-x-1/2 right-1/2'>
+            <div className='absolute bg-blue-400 w-full aspect-video max-w-[100vw] lg:max-w-screen-lg translate-y-1/2 bottom-1/2 translate-x-1/2 right-1/2'>
+              <Sketch />
+            </div>
           </div>
         </div>
       </div>
