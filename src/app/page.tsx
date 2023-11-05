4ad6be9ee790605c53f8fdb4f2ec62f9e632aa0e
egmp7 <deweyerick@gmail.com>
2023-11-05 13:33:45 -0600

    delete post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,21 @@ export default async function Home() {
 
   const posts =  await prisma.post.findMany();
 
+  async function deletePost(id:string){
+    "use server"
+
+    const post = await prisma.post.delete({where:{
+      id:id
+    }})
+
+    console.log(post)
+  }
+
   return (<>
 
     <h1>Prisma Post</h1>
     <div>{posts.map((post) => (
-        <PostItem key={post.id} {...post}  />
+        <PostItem key={post.id} {...post} deletePost={deletePost} />
     ))}</div>
 
   </>)
