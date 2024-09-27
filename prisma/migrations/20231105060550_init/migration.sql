a5914b437f02538321553449cecc5fd2628f437e
egmp7 <deweyerick@gmail.com>
2024-09-27 12:51:37 -0500

    v2 Fresh Start

--- prisma/migrations/20231105060550_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Post" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "content" TEXT,
-    "published" BOOLEAN NOT NULL DEFAULT false,
-    "createdOn" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedOn" DATETIME NOT NULL
-);
