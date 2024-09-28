eb829e24849a1ad6956a34065bb587b75edef176
egmp7 <deweyerick@gmail.com>
2024-09-28 14:57:08 -0500

    about page

---
+++ app/about/messages.tsx
@@ -0,0 +1,8 @@
+const messages ={
+    title: "About me",
+    descriptionOne: "I began my coding journey in 2018, teaching myself Python and creating my first animation programs using the Turtle module. It sparked a passion for programming that only grew as I explored more technologies. I expanded my skills by learning JavaScript, PHP, and Laravel, building my first e-commerce stores and dynamic websites.",
+    descriptionTwo: "In 2021, I enrolled at the University of London, where I honed my abilities in computer fundamentals, algorithms, security, hardware, SQL, machine learning, AI, computer vision, and video game development. I have also developed real-time applications for the student community using the Slack SDK.",
+    descriptionThree: "With a strong work ethic and a dedication to continuous learning, I’m always excited to collaborate on projects that align with my values and skills. Let’s build something great together!",
+}
+
+export default messages;
